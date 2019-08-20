946b47d4a703aa4eb397035bab767537ca217e03
tomspost <tomc@3gmagic.com>
2019-08-20 12:21:40 +0100

    Update mag3110cal.js

--- mag3110cal.js
+++ mag3110cal.js
@@ -1,1 +1,19 @@
 // working code to go here!
+var Vec3 = require('Vec3');
+
+//lunch needed bofre continuing...
+
+function calibrate () {
+  
+  
+}
+
+
+function mag3110Cal (doCal) {
+  
+  
+  
+};
+
+
+exports.mag3110Cal;
